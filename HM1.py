d0f59a9c2fa2c30b239e8401925f770ae36606d1
SergeyVeletnyuk <sergeyveletnyuk57@gmail.com>
2024-01-31 19:25:57 +0300

    done

--- HM1.py
+++ HM1.py
@@ -4,6 +4,7 @@ import subprocess
 def find_subprocess(path: str, text: str):
     result = subprocess.run(path, shell=True, stdout=subprocess.PIPE, encoding="utf-8")
     lst = result.stdout.split("\n")
+    print(lst)
     if result.returncode == 0:
         if text in lst:
             return True
--- Pytests/test_lesson2.py
+++ Pytests/test_lesson2.py
@@ -1,10 +1,13 @@
-import subprocess
 from HM1_2 import find_subprocess
 import zlib
+import yaml
 
-tst = "/home/ubuntu/tst"
-out = "/home/ubuntu/out"
-folder1 = "/home/ubuntu/folder1"
+
+
+with open("config.yaml") as f:
+    data = yaml.safe_load(f)
+
+
 
 def crc32(cmd):
     with open(cmd, 'rb') as g:
@@ -17,37 +20,39 @@ def crc32(cmd):
         return "%08X" % (hash & 0xFFFFFFFF)
 
 
-def test_step1():
-    res1 = find_subprocess(f"cd {tst}; 7z a {out}/arx2", "Everything is Ok")
-    res2 = find_subprocess(f"ls {out}", "arx27z")
-    assert res1 and res2, "test1 FAIL"
+class Test_positiv:
+    def test_step1(self, make_folders, clear_folders, make_files):
+        res1 = find_subprocess(f"cd {data['tst']}; 7z a -t{data['config_type']} {data['out']}/arx2", "Everything is Ok")
+        res2 = find_subprocess(f"ls {data['out']}", "")
+        assert res1 and res2, "test1 FAIL"
+
+    def test_step2(self, clear_folders, make_files):
+        res = []
+        res.append(find_subprocess(f"cd {data['tst']}; 7z a -t{data['config_type']} {data['out']}/arx2", "Everything is Ok"))
+        res.append(find_subprocess(f"cd {data['out']}; 7z e arx2.{data['config_type']} -o{data['folder1']} -y", "Everything is Ok"))
+        for item in make_files:
+            res.append(find_subprocess(f"ls {data['folder1']}", item))
+
+        assert all(res), "test2 FAIL"
+
+    def test_step3(self):
+        assert find_subprocess(f"cd {data['out']}; 7z l arx2.{data['config_type']}", f"{data['count']}")
+
+    def test_step4(self):
+        assert find_subprocess(f"cd {data['out']}; 7z t arx2.{data['config_type']}", "Everything is Ok"), "test4 FAIL"
+
+    def test_step5(self):
+        assert find_subprocess(f"cd {data['out']}; 7z d arx2.7z", "Everything is Ok"), "test5 FAIL"
+
+    def test_step6(self):
+        assert find_subprocess(f"cd {data['out']}; 7z u arx2.7z", "Everything is Ok"), "test6 FAIL"
+
+    def test_step7(self, clear_folders, make_files):
+        find_subprocess(f"cd {data['tst']}; 7z a {data['out']}/arx2", "Everything is Ok")
+        assert find_subprocess(f"cd {data['out']} && 7z x arx2.7z -o{data['folder1']}", "Everything is Ok")
+
+    def test_step8(self):
+        res1 = crc32(f"{data['out']}/arx2.7z").lower()
+        assert find_subprocess(f"crc32 {data['out']}/arx2.7z", res1), "test8 FAIL"
 
 
-def test_step2():
-    res1 = find_subprocess(f"cd {out}; 7z e arx2.7z -o{folder1} -y", "Everything is Ok")
-    res2 = find_subprocess(f"ls {folder1}", "testtxt")
-    assert res1 and res2, "test2 FAIL"
-
-
-def test_step3():
-    assert find_subprocess(f"cd {out}; 7z l out.7z", "1 files")
-
-
-
-def test_step4():
-    assert find_subprocess(f"cd {out}; 7z t arx2.7z", "Everything is Ok"), "test4 FAIL"
-
-
-def test_step5():
-    assert find_subprocess(f"cd {out}; 7z d arx2.7z", "Everything is Ok"), "test5 FAIL"
-
-
-def test_step6():
-    assert find_subprocess(f"cd {out}; 7z u arx2.7z", "Everything is Ok"), "test6 FAIL"
-
-def test_step7():
-    assert find_subprocess(f"cd {out} && 7z x /home/ubuntu/out/out1/out.7z", "Everything is Ok")
-
-def test_step8():
-    res1 = crc32(f"{out}/out.7z").lower()
-    assert find_subprocess(f"crc32 {out}/out.7z", res1), "test8 FAIL"
